16a830a6d06c5658504d0d78857155e02dfdf69c
Revelol <thakurj2000@gmail.com>
2019-03-10 13:06:07 -0400

    created the 3 panels (top, stats, and results)
    added images to program
    
    created interface

--- src/RockPaperScissorsFrame.java
+++ src/RockPaperScissorsFrame.java
@@ -6,13 +6,14 @@ import java.util.Random;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 
-public class RockPaperScissorsFrame extends JFrame {
+public class RockPaperScissorsFrame extends JFrame implements Strategy {
     JPanel main, top, result, stats;
     JLabel lUserWins, lComputerWins, lDraws;
     JButton rockBtn, paperBtn, scissorsBtn, quitBtn;
     JTextArea results;
     JScrollPane scrollPane;
-    int iUserWins, iComputerWins, iDraws;
+    int iUserWins, iComputerWins, iDraws, playerChoice, computerChoice;
+
 
     public RockPaperScissorsFrame()
     {
@@ -51,18 +52,36 @@ public class RockPaperScissorsFrame extends JFrame {
         ImageIcon temp = new ImageIcon(this.getClass().getResource("rocksign.jpg"));
         ImageIcon rocksign = new ImageIcon(temp.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
         rockBtn.setIcon(rocksign);
+
+        rockBtn.addActionListener((ActionEvent ae) -> {
+            playerChoice = 0;
+            determineMove();
+        });
+
         top.add(rockBtn);
         //paper button
         paperBtn = new JButton();
         ImageIcon temp1 = new ImageIcon(this.getClass().getResource("handsign.jpg"));
         ImageIcon handsign = new ImageIcon(temp1.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
         paperBtn.setIcon(handsign);
+
+        paperBtn.addActionListener((ActionEvent ae) -> {
+            playerChoice =1;
+            determineMove();
+        });
+
         top.add(paperBtn);
         //scissors  button
         scissorsBtn = new JButton();
         ImageIcon temp2 = new ImageIcon(this.getClass().getResource("scissorssign.jpg"));
         ImageIcon scissorssign = new ImageIcon(temp2.getImage().getScaledInstance(50,50,Image.SCALE_SMOOTH));
         scissorsBtn.setIcon(scissorssign);
+
+        scissorsBtn.addActionListener((ActionEvent ae) -> {
+            playerChoice =2;
+            determineMove();
+        });
+
         top.add(scissorsBtn);
 
     }
@@ -87,5 +106,17 @@ public class RockPaperScissorsFrame extends JFrame {
         lDraws = new JLabel(""+iDraws);
         stats.add(lDraws);
     }
+    public void determineMove(){
+        determineComputerMove();
+        determineResult();
+    }
 
+    private void determineComputerMove() {
+        Random r = new Random();
+        computerChoice = r.nextInt(3);
+
+    }
+    private void determineResult(){
+        
+    }
 }
